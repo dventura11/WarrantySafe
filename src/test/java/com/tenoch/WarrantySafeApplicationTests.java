89c2ea4e784adf3150fe6a4b5e85ded7109e3355
jonanavarro89 <ing.jonathan.navarro@gmail.com>
2016-05-18 18:04:47 -0500

    Estructura básica

---
+++ src/test/java/com/tenoch/WarrantySafeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tenoch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WarrantySafeApplication.class)
+public class WarrantySafeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
