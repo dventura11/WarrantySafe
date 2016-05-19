6ef593fffe12964f9fa8400f090eb559871d8739
jonanavarro89 <ing.jonathan.navarro@gmail.com>
2016-05-18 22:37:38 -0500

    Estructura básica del proyecto.

---
+++ src/main/java/com/tenoch/WarrantySafeApplication.java
@@ -0,0 +1,12 @@
+package com.tenoch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantySafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarrantySafeApplication.class, args);
+	}
+}
